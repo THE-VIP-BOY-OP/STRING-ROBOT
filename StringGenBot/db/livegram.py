3982f192b40ed2aa69c753a52bb3193f1cbc886a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-05-28 16:11:36 +0530

    Update livegram.py

--- StringGenBot/db/livegram.py
+++ StringGenBot/db/livegram.py
@@ -1,11 +1,11 @@
-from pyrogram import Client, filters
+from pyrogram import app, client, filters
 from pyrogram.types import Message
 
 from config import OWNER_ID
 
 
 
-@Client.on_message(filters.private & filters.incoming)
+@app.on_message(filters.private & filters.incoming)
 async def on_pm_s(client: Client, message: Message):
     if not message.from_user.id ==OWNER_ID:
         fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
